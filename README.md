79e6ce6dd4a40a70829543cfad405d722738a43c
taosiyu <taosiyu19@126.com>
2017-07-31 12:02:27 +0800

    粒子效果
    粒子效果

--- README.md
+++ README.md
@@ -1,32 +1,41 @@
 
 
 
+ ![image](https://github.com/taosiyu/Rain-Animation/raw/master/images/rain.png)
+
+
 将平时自己自己的动画放在这里 ，并且附上源码 
 
 目标是100！
 
+目前7/100 
+
 支持swift3.0
 
-##3d方块
+ ##1.3d方块
 
  ![image](https://github.com/taosiyu/Rain-Animation/raw/master/images/one.gif)
 
-##下拉刷新（渐变色）
+ ##2.下拉刷新（渐变色）
 
  ![image](https://github.com/taosiyu/Rain-Animation/raw/master/images/two.gif)
 
-##比例图
+ ##3.比例图
 
  ![image](https://github.com/taosiyu/Rain-Animation/raw/master/images/three.gif)
 
-##柱状图
+ ##4.柱状图
 
  ![image](https://github.com/taosiyu/Rain-Animation/raw/master/images/four.gif)
 
-##加载动画
+ ##5.加载动画
 
  ![image](https://github.com/taosiyu/Rain-Animation/raw/master/images/five.gif)
 
-##水状动画
+ ##6.水状动画
 
  ![image](https://github.com/taosiyu/Rain-Animation/raw/master/images/six.gif)
+
+ ##7.🌰效果
+
+ ![image](https://github.com/taosiyu/Rain-Animation/raw/master/images/seven.gif)
--- RainAnimation/RainAnimation/Animations/Emitter/EmitterCtr.swift
+++ RainAnimation/RainAnimation/Animations/Emitter/EmitterCtr.swift
@@ -66,8 +66,6 @@ class EmitterCtr: UIViewController {
         emitter1.emitterCells = [cell]
         
         //2.下雪
-        
-        
         let emitter2 = CAEmitterLayer.init()
         emitter2.frame = CGRect(x: 150, y: 200, width: 210, height: 120)
         self.containerView.layer.addSublayer(emitter2)
@@ -96,6 +94,61 @@ class EmitterCtr: UIViewController {
         
         emitter2.emitterCells = [cell2]
         
+        
+        //3.烟花
+        let emitter3 = CAEmitterLayer.init()
+        emitter3.renderMode = kCAEmitterLayerAdditive
+        emitter3.emitterMode = kCAEmitterLayerOutline
+        emitter3.emitterShape = kCAEmitterLayerLine
+        emitter3.emitterPosition = CGPoint(x: self.containerView.layer.bounds.width*0.5, y: self.containerView.layer.bounds.height)
+        emitter3.emitterSize = CGSize(width: self.containerView.layer.bounds.width*0.5, height: 0)
+        emitter3.seed = (arc4random()%100)+1
+        
+        let rocket = CAEmitterCell()
+        rocket.birthRate		= 1.0
+        rocket.emissionRange	= CGFloat(0.25 * Double.pi)
+        rocket.velocity			= 280
+        rocket.velocityRange	= 100
+        rocket.yAcceleration	= 75
+        rocket.lifetime			= 1.02
+        rocket.contents			= self.getImageWithColor(color: UIColor.black, width: 20).cgImage
+        rocket.scale			= 0.2
+        rocket.color			= UIColor.red.cgColor
+        rocket.greenRange		= 1.0
+        rocket.redRange			= 1.0
+        rocket.blueRange		= 1.0
+        rocket.spinRange		= CGFloat(Double.pi)
+        
+        let cell3 = CAEmitterCell()
+        cell3.birthRate			= 1.0
+        cell3.velocity			= 0
+        cell3.scale				= 2.5
+        cell3.redSpeed			= cell3.redSpeed-1.5
+        cell3.blueSpeed			= cell3.blueSpeed+1.5
+        cell3.greenSpeed		= cell3.greenSpeed+1.0
+        cell3.lifetime			= 0.35
+        
+        
+        let cell4 = CAEmitterCell()
+        cell4.birthRate			= 400
+        cell4.velocity			= 125
+        cell4.emissionRange		= CGFloat(2.0*Double.pi)
+        cell4.yAcceleration		= 75
+        cell4.lifetime			= 3
+        cell4.contents			= self.getImageWithColor(color: UIColor.brown, width: 20).cgImage
+        cell4.scaleSpeed		= cell4.scaleSpeed-0.2
+        cell4.greenSpeed		= cell4.greenSpeed-0.1
+        cell4.redSpeed			= 0.4
+        cell4.blueSpeed			= cell4.blueSpeed-0.1
+        cell4.alphaSpeed		= cell4.alphaSpeed-0.25
+        cell4.spin				= CGFloat(2.0*Double.pi)
+        cell4.spinRange			= CGFloat(2.0*Double.pi)
+        
+        emitter3.emitterCells = [rocket]
+        rocket.emitterCells = [cell3]
+        cell3.emitterCells = [cell4]
+        self.containerView.layer.addSublayer(emitter3)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -103,8 +156,11 @@ class EmitterCtr: UIViewController {
         
     }
     
-    func getImageWithColor(color:UIColor)->UIImage{
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+    func getImageWithColor(color:UIColor,width:CGFloat=0)->UIImage{
+        var rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        if width > 0{
+           rect = CGRect(x: 0, y: 0, width: width, height: width)
+        }
         UIGraphicsBeginImageContext(rect.size)
         let context = UIGraphicsGetCurrentContext()
         context!.setFillColor(color.cgColor)
@@ -126,3 +182,73 @@ class EmitterCtr: UIViewController {
     */
 
 }
+
+
+//MARK:这里是一些属性的介绍
+//alphaRange:  一个粒子的颜色alpha能改变的范围；
+//
+//alphaSpeed:粒子透明度在生命周期内的改变速度；
+//
+//birthrate：粒子参数的速度乘数因子；
+//
+//blueRange：一个粒子的颜色blue 能改变的范围；
+//
+//blueSpeed: 粒子blue在生命周期内的改变速度；
+//
+//color:粒子的颜色
+//
+//contents：是个CGImageRef的对象,既粒子要展现的图片；
+//
+//contentsRect：应该画在contents里的子rectangle：
+//
+//emissionLatitude：发射的z轴方向的角度
+//
+//emissionLongitude:x-y平面的发射方向
+//
+//emissionRange；周围发射角度
+//
+//emitterCells：粒子发射的粒子
+//
+//enabled：粒子是否被渲染
+//
+//greenrange: 一个粒子的颜色green 能改变的范围；
+//
+//greenSpeed: 粒子green在生命周期内的改变速度；
+//
+//lifetime：生命周期
+//
+//lifetimeRange：生命周期范围
+//
+//magnificationFilter：不是很清楚好像增加自己的大小
+//
+//minificatonFilter：减小自己的大小
+//
+//minificationFilterBias：减小大小的因子
+//
+//name：粒子的名字
+//
+//redRange：一个粒子的颜色red 能改变的范围；
+//
+//redSpeed; 粒子red在生命周期内的改变速度；
+//
+//scale：缩放比例：
+//
+//scaleRange：缩放比例范围；
+//
+//scaleSpeed：缩放比例速度：
+//
+//spin：子旋转角度
+//
+//spinrange：子旋转角度范围
+//
+//style：不是很清楚：
+//
+//velocity：速度
+//
+//velocityRange：速度范围
+//
+//xAcceleration:粒子x方向的加速度分量
+//
+//yAcceleration:粒子y方向的加速度分量
+//
+//zAcceleration:粒子z方向的加速度分量
